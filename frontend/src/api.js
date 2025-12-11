be3137872bc6402e564a8d90bf1f93332ab34680
Satyam Kamal <stym4019@gmail.com>
2025-12-11 13:17:33 +0530

    CORS and CSRF fixed for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const api = axios.create({
-  //baseURL: "http://127.0.0.1:8000/api",
-  baseURL: "https://fullstack-docs-project.onrender.com/api",
-});
+const baseURL =
+  window.location.hostname === "localhost"
+    ? import.meta.env.VITE_API_BASE_LOCAL
+    : import.meta.env.VITE_API_BASE_PROD;
+
+export const api = axios.create({ baseURL });
